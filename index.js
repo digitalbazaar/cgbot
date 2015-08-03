42e4250d89bdc36224fddbe17fb52bf58098b37b
Manu Sporny <msporny@digitalbazaar.com>
2015-08-03 18:14:39 +0000

    Add queue management.

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ process.on('SIGINT', function() {
 
 // shared variables
 var participants = {};
+var speakerQueue = [];
 var ircChannel = '#' + config.irc.channel;
 var asteriskClient;
 
@@ -112,12 +113,63 @@ ircClient.on('join', function(channel, nick, message) {
 
   // listen to IRC channel messages
   ircClient.on('message#' + config.irc.channel, function(nick, message) {
+    // handle non-voipbot directed channel commands
+    var args = message.split(' ');
+    var command = args.shift();
+    if(command.search(/^q\+/) === 0) {
+      var queueEntry = {};
+      var effectiveNick = nick;
+      if(args.length === 1) {
+        effectiveNick = args[0]; 
+      } else if(args.length > 1) {
+        queueEntry.reminder = args.join(' ');
+      }
+      queueEntry.nick = effectiveNick;
+      speakerQueue.push(queueEntry);
+      say(nick + ' has been added to the queue: ' +
+        queueToString(speakerQueue));
+    }
+    if(command.search(/^ack/) === 0 && args.length === 1) {
+      var speaker = removeFromQueue(speakerQueue, args[0]);
+      if(speaker) {
+        var reminder = '';
+        if(speaker.reminder) {
+          reminder = ' (' + speaker.reminder + ')';
+        }
+        say(speaker.nick + ' has the floor' + reminder + '.');
+      } else {
+        say(args[0] + ' isn\'t on the speaker queue.');
+      }
+    }
+    if(command.search(/^q\-/) === 0) {
+      var effectiveNick = nick;
+      if(args.length > 0) {
+        effectiveNick = args[0];
+      }
+      var speaker = removeFromQueue(speakerQueue, effectiveNick);
+      if(speaker) {
+        say(speaker.nick + ' has been removed from the queue: ' +
+          queueToString(speakerQueue));
+      } else {
+        say(args[0] + ' isn\'t on the speaker queue.');
+      }
+    }
+    if(command.search(/^q\?/) === 0 && args.length === 0) {
+      if(speakerQueue.length === 0) {
+        say('The speaker queue is empty.');
+      } else {
+        say('The current speaker queue is: ' + queueToString(speakerQueue));
+      }
+    }
+
+    // handle voipbot specific commands
+
     var voipbotRegex = new RegExp('^voip.*:', 'i');
     if(!voipbotRegex.test(message)) {
       return;
     }
 
-    var args = message.split(' ');
+    args = message.split(' ');
     // all commands must contain at least the command name which is
     // the second argument
     if(args.length < 2) {
@@ -125,7 +177,7 @@ ircClient.on('join', function(channel, nick, message) {
     }
 
     args.shift();
-    var command = args.shift();
+    command = args.shift();
 
     // show list of participants
     var channel = 'unknown';
@@ -225,6 +277,34 @@ ircClient.on('join', function(channel, nick, message) {
 });
 
 /*************************** Helper functions ******************************/
+
+/**
+ * Converts a queue to a string.
+ *
+ * @param queue the queue to convert to a string
+ * @return a string representing the queue
+ */
+function queueToString(queue) {
+  return (queue.map(function(queueEntry) { return queueEntry.nick }))
+    .join(', ');
+};
+
+/**
+ * Removes a given nick from a queue.
+ *
+ * @param queue the queue to modify.
+ * @param nick the nickname to remove from the queue
+ *
+ * @return the removed value.
+ */
+function removeFromQueue(queue, nick) {
+  for(var i = 0; i < queue.length; i++) {
+    if(queue[i].nick === nick) {
+      return queue.splice(i, 1)[0];
+    }
+  }
+  return null;
+};
 
 /**
  * Pretty-prints a channel name for human readability.
