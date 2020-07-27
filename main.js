a27cf15028df84af68d65f9c7bfbd35225df7917
Manu Sporny <msporny@digitalbazaar.com>
2020-07-26 23:09:55 -0400

    Refactor channel management in IRC.

--- main.js
+++ main.js
@@ -2,4 +2,4 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 export {monitor} from './lib/monitor.js';
-//export {manage} from './lib/manage.js';
+export {manage} from './lib/manage.js';
